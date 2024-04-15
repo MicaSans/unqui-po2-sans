2f2c3eb0514764cbf93caeb5516bad7ee9102863
MicaSans <mica.sans@hotmail.com>
2024-04-15 01:47:26 -0300

    Agrego TP3-Ej5.4

--- test/ar/edu/unq/po2/tp3/TiposPrimitivosTest.java
+++ test/ar/edu/unq/po2/tp3/TiposPrimitivosTest.java
@@ -9,9 +9,18 @@ class TiposPrimitivosTest {
 	private Integer enteroInteger;
 
 	@Test
-	void testNumeros() {
-		System.out.println(enteroInt);
-		System.out.println(enteroInteger);
+	void testNumerosEnInstancia() {
+		System.out.println("Valor predeterminado en variable de instancia int: " + enteroInt);
+		System.out.println("Valor predeterminado en variable de instancia Integer: " + enteroInteger);
 	}
-
+	
+	@Test
+	public void testNumerosEnMetodo() {
+		//Variables de metodos
+		int numeroInt = enteroInt;
+		Integer numeroInteger = enteroInteger;
+		//Al ser variables de metodos si o si tengo que inicializarla, no puede ser valor predeterminado, por lo que la inicialice con las variables de instancias creadas para el test anterior
+		System.out.println("Valor predeterminado en variable de metodo int: " + numeroInt);
+		System.out.println("Valor predeterminado en variable de metodo Integer: " + numeroInteger);
+	}
 }
