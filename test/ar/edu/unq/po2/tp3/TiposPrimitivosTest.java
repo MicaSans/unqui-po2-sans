120c27338aa17f30da7194a68f22c44da937c979
MicaSans <mica.sans@hotmail.com>
2024-04-15 01:32:34 -0300

    Agrego TP3-Ej5

---
+++ test/ar/edu/unq/po2/tp3/TiposPrimitivosTest.java
@@ -0,0 +1,17 @@
+package ar.edu.unq.po2.tp3;
+
+import org.junit.jupiter.api.Test;
+
+class TiposPrimitivosTest {
+
+	//Variables de instancia
+	private int enteroInt;
+	private Integer enteroInteger;
+
+	@Test
+	void testNumeros() {
+		System.out.println(enteroInt);
+		System.out.println(enteroInteger);
+	}
+
+}
