97fc79b1627d2247af86ace672c27183672f8b67
MicaSans <mica.sans@hotmail.com>
2024-04-22 01:52:33 -0300

    Agrego TP4-Ejercicio2

--- src/ar/edu/unq/po2/tp4/ProductoPrimeraNecesidad.java
+++ src/ar/edu/unq/po2/tp4/ProductoPrimeraNecesidad.java
@@ -2,10 +2,37 @@ package ar.edu.unq.po2.tp4;
 
 public class ProductoPrimeraNecesidad extends Producto {
 	
-	//Constructor
-	public ProductoPrimeraNecesidad(String nombreDeProducto, int precioDeProducto, boolean esPreciosCuidados) {
-		super(nombreDeProducto, precioDeProducto, esPreciosCuidados);
+	//Variable de instancia
+	private int descuento;
+	
+	//Constructor con valor de preciosCuidados por defecto false
+	public ProductoPrimeraNecesidad(String nombreDeProducto, double precioDeProducto) {
+		super(nombreDeProducto, precioDeProducto);
 		this.setPrecio(precioDeProducto * 0.9);
 	}
 
+	//Constructor que permite especificar preciosCuidados para alguno que lo sea
+	public ProductoPrimeraNecesidad(String nombreDeProducto, double precioDeProducto, boolean esPreciosCuidados) {
+		super(nombreDeProducto, precioDeProducto, esPreciosCuidados);
+		this.setPrecio(precioDeProducto * 0.9);
+	}
+	
+	//Agrego constructor con valor de preciosCuidados por defecto false que permite especificar descuento a aplicar para punto 2
+	public ProductoPrimeraNecesidad(String nombreDeProducto, double precioDeProducto, int descuentoAAplicar) {
+		super(nombreDeProducto, precioDeProducto);
+		this.descuento = descuentoAAplicar;
+	}
+	
+	//Agrego constructor que permite especificar preciosCuidados y descuento a aplicar para punto 2
+	public ProductoPrimeraNecesidad(String nombreDeProducto, double precioDeProducto, boolean esPreciosCuidados, int descuentoAAplicar) {
+		super(nombreDeProducto, precioDeProducto, esPreciosCuidados);
+		this.descuento = descuentoAAplicar;
+	}
+	
+	//Metodos
+	@Override
+	public double getPrecio() {
+		double precioConDescuento = super.getPrecio() - ((super.getPrecio() * this.descuento) / 100);
+		return precioConDescuento;
+	}
 }
--- test/ar/edu/unq/po2/tp4/ProductoPrimeraNecesidadTest.java
+++ test/ar/edu/unq/po2/tp4/ProductoPrimeraNecesidadTest.java
@@ -8,14 +8,17 @@ import static org.junit.jupiter.api.Assertions.*;
 public class ProductoPrimeraNecesidadTest {
 
 	private ProductoPrimeraNecesidad leche;
+	private ProductoPrimeraNecesidad azucar;
 	
 	@BeforeEach
 	public void setUp() {
 		leche = new ProductoPrimeraNecesidad("Leche", 8, false);
+		azucar = new ProductoPrimeraNecesidad("azucar", 1000, 50);
 	}
 	
 	@Test
 	public void testCalcularPrecio() {
 		assertEquals(7.2, leche.getPrecio());
+		assertEquals(500, azucar.getPrecio());
 	}
 }
